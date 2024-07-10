cf989d7f2dc3e859cf318fd0f9e2e034960e8edf
Abdul Ahad <147072622+AbdulAhad-2005@users.noreply.github.com>
2024-07-10 15:34:25 +0500

    Update AddingPartyImages.java

--- VotingSystem/AddingPartyImages.java
+++ VotingSystem/AddingPartyImages.java
@@ -6,20 +6,22 @@ import java.sql.*;
 public class AddingPartyImages {
 
     public static void main(String[] args) {
-        File imageFile1 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\pti.png");
-        File imageFile2 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\plmn.png");
-        File imageFile3 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\ppp.png");
-        File imageFile4 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\juif.png");
-        File imageFile5 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\mqm.png");
-        File imageFile6 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\jamat-e-islami.png");
+// Add path to your own images that contain symbols of parties. These are just for understanding
+        // File imageFile1 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\pti.png");
+        // File imageFile2 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\plmn.png");
+        // File imageFile3 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\ppp.png");
+        // File imageFile4 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\juif.png");
+        // File imageFile5 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\mqm.png");
+        // File imageFile6 = new File("C:\\Users\\abdul\\OneDrive\\Pictures\\Voting System Project pics\\jamat-e-islami.png");
 
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/voting_system", "root", "root")) {
-            insertImage(connection, imageFile1, "Pakistan Tehreek-e-Insaf");
-            insertImage(connection, imageFile2, "Pakistan Muslim League-Nawaz");
-            insertImage(connection, imageFile3, "Pakistan Peoples Party");
-            insertImage(connection, imageFile4, "Jamiat Ulema-e-Islam (F)");
-            insertImage(connection, imageFile5, "Muttahida Qaumi Movement");
-            insertImage(connection, imageFile6, "Jamat-e-Islami");
+// change the name of the parties in the below insertImage function calls to the parties you want in your voting system.
+            // insertImage(connection, imageFile1, "Pakistan Tehreek-e-Insaf");
+            // insertImage(connection, imageFile2, "Pakistan Muslim League-Nawaz");
+            // insertImage(connection, imageFile3, "Pakistan Peoples Party");
+            // insertImage(connection, imageFile4, "Jamiat Ulema-e-Islam (F)");
+            // insertImage(connection, imageFile5, "Muttahida Qaumi Movement");
+            // insertImage(connection, imageFile6, "Jamat-e-Islami");
 
             System.out.println("Images inserted successfully.");
         } catch (SQLException | IOException e) {
